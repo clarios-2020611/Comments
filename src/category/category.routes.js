f7d74ba1f91274c4b396cae5062b9bdebf75b2a2
clarios-2020611 <clarios-2020611@kinal.edu.gt>
2025-02-21 11:42:04 -0600

    Se agregan las validaciones ya hechas en las rutas

--- src/category/category.routes.js
+++ src/category/category.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { createCategory, deletedCat, getAll, getOne, updateCat } from "./category.controller";
-import { createCategoryValidators, needId, updatedCategory } from "../../middlewares/validators";
+import { createCategory, deletedCat, getAll, getOne, updateCat } from "./category.controller.js";
+import { createCategoryValidators, needId, updatedCategory } from "../../middlewares/validators.js";
 
 const api = Router();
 
@@ -9,3 +9,5 @@ api.get('/getAll', getAll);
 api.get('/getOne', needId, getOne);
 api.put('/updateCategory', updatedCategory, updateCat);
 api.put('/deletedCategory', needId, deletedCat);
+
+export default api;
