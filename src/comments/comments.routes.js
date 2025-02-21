39b1ba023763f4ccb9ebb7088099f14a598bfc89
clarios-2020611 <clarios-2020611@kinal.edu.gt>
2025-02-21 11:43:28 -0600

    Se crean las rutas faltando validaciones para el update

--- src/comments/comments.routes.js
+++ src/comments/comments.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { createPost, deletedPost, getAll, getOne, updatePost } from "./comments.controller";
-import { needId } from "../../middlewares/validators";
+import { createPost, deletedPost, getAll, getOne, updatePost } from "./comments.controller.js";
+import { createPostValidator, needId } from "../../middlewares/validators.js";
 
 const api = Router();
 
-api.post('/createPost', createPost);
+api.post('/createPost', createPostValidator ,createPost);
 api.get('/getAll', getAll);
 api.get('/getOne', needId, getOne);
 api.put('/updatedPost', updatePost);
